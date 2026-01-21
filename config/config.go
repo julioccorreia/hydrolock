5de1ee794537af192b21554900e95dde3b5a20a3
Julio <juliocgazige@hotmail.com>
2026-01-21 14:42:01 -0300

    feat(infra): implement config loader using viper

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	HTTPPort     string `mapstructure:"HTTP_PORT"`
+	DBHost       string `mapstructure:"DB_HOST"`
+	DBPort       string `mapstructure:"DB_PORT"`
+	DBUser       string `mapstructure:"DB_USER"`
+	DBPassword   string `mapstructure:"DB_PASSWORD"`
+	DBName       string `mapstructure:"DB_NAME"`
+	GeminiAPIKey string `mapstructure:"GEMINI_API_KEY"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return nil, err
+		}
+	}
+
+	var cfg Config
+
+	err := viper.Unmarshal(&cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
