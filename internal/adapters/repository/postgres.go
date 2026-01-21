6295e8571c67d9ab5a804049e0e7909bd40bd8f6
Julio <juliocgazige@hotmail.com>
2026-01-21 16:18:31 -0300

    style(log): change messages to english

--- internal/adapters/repository/postgres.go
+++ internal/adapters/repository/postgres.go
@@ -17,12 +17,12 @@ func NewPostgresDB(cfg *config.Config) (*gorm.DB, error) {
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		return nil, fmt.Errorf("Falha ao conectar no postgres: %w", err)
+		return nil, fmt.Errorf("failed to connect to postgres: %w", err)
 	}
 
 	err = db.AutoMigrate(&domain.WaterIntake{})
 	if err != nil {
-		return nil, fmt.Errorf("Falha na migração automática: %w", err)
+		return nil, fmt.Errorf("failed to run auto-migration: %w", err)
 	}
 
 	return db, nil
